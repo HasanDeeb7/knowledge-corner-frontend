8077a785428a5e52391c4edd498598171cab7a79
Hasan Deeb <sektories7@gmail.com>
2024-01-04 22:22:22 +0200

    fixed pagination design

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,47 +1,80 @@
 import style from "./Pagination.module.css";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import { useState } from "react";
 
 function Pagination({ books, setPagination, pagination }) {
   const limits = pagination.pageSize;
+  const [error, setError] = useState();
   function getTotal() {
     return Math.ceil(books.length / limits);
   }
+  // const handleChange = (event) => {
+  //   setMessage(event.target.value);
+  // };
 
-  function generatePageNumbers() {
-    const totalPages = getTotal();
-    const currentPage = pagination.pageNumber;
-    const pageNumbers = [];
-
-    for (let i = 1; i <= Math.min(totalPages, limits); i++) {
-      pageNumbers.push(i);
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      if (event.target.value <= getTotal()) {
+        setPagination({ ...pagination, pageNumber: event.target.value });
+      } else {
+        setError(`Last Page is ${getTotal()}`);
+      }
     }
-
-    if (totalPages > limits) {
-      pageNumbers.push("...");
-
-      pageNumbers.push(totalPages);
-    }
-
-    return pageNumbers;
-  }
+  };
 
   return (
     <div className={style.paginationContainer}>
-      {generatePageNumbers().map((item, idx) => {
-        return item !== "..." ? (
-          <li
-            className={style.paginationElement}
-            value={item}
-            key={idx}
-            onClick={(e) =>
-              setPagination({ ...pagination, pageNumber: e.target.value })
+      <div className={style.paginationControlContainer}>
+        <li
+          className={`${style.paginationElement} ${
+            pagination.pageNumber <= 1 && style.disabledPaginationArrow
+          }`}
+        >
+          <ChevronLeftRoundedIcon
+            className={`${style.paginaitonArrow} ${
+              pagination.pageNumber <= 1 && style.disabledPaginationArrow
+            }`}
+            disabled={pagination.pageNumber <= 1}
+            onClick={() =>
+              setPagination({
+                ...pagination,
+                pageNumber: pagination.pageNumber - 1,
+              })
             }
-          >
-            {item}
-          </li>
-        ) : (
-          item
-        );
-      })}
+          />
+        </li>
+        <li className={style.paginationElement}>{pagination.pageNumber}</li>
+        <li
+          className={`${style.paginationElement} ${
+            pagination.pageNumber >= getTotal() && style.disabledPaginationArrow
+          }`}
+        >
+          <ChevronRightRoundedIcon
+            className={style.paginaitonArrow}
+            disabled={pagination.pageNumber >= getTotal()}
+            onClick={() =>
+              setPagination({
+                ...pagination,
+                pageNumber: pagination.pageNumber + 1,
+              })
+            }
+          />
+        </li>
+      </div>
+      <div className={style.jumpToPageContainer}>
+        <label htmlFor="gotoPage">Jump To</label>
+        <input
+          type="number"
+          name="gotoPage"
+          id="gotoPage"
+          min={0}
+          max={getTotal()}
+          className={style.jumpToPageInput}
+          onKeyDown={handleKeyDown}
+        />
+        {error && <span className={style.errorSpan}> {error}</span>}
+      </div>
     </div>
   );
 }
